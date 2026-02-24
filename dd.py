184d1def7b641762664dea49061a82ed512c553f
mackung112 <74304923+mackung112@users.noreply.github.com>
2026-02-24 09:53:22 +0700

    เพิ้มเลข

--- dd.py
+++ dd.py
@@ -1,1 +1,7 @@
 print("l;ylfu")
+#โปรแกรมคำนวณเลข ฐานสอง
+def convert_to_binary(n):
+    return bin(n).replace("0b", "")
+
+number = int(input("กรุณาป้อนเลขฐานสิบ: "))
+print(f"เลขฐานสองคือ: {convert_to_binary(number)}")
